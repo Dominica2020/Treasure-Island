fa69411c6f77eb69802b8ee52fe12bc4713d316b
Dominica2020 <56567205+Dominica2020@users.noreply.github.com>
2022-03-13 17:57:53 -0700

    Update proj3.py

--- proj3.py
+++ proj3.py
@@ -19,17 +19,17 @@ print("")
 print("Your mission is to find the treasure.")
 print("")
 
-choice_1 = input("You're at a fork in the road. Where do you want to go?\nType \"left\" or \"right\". ")
+choice_1 = input("You're at a fork in the road. Where do you want to go?\nType \"left\" or \"right\".\n")
 
 if choice_1.lower() == "left":
-  choice_2 = input("You've come to a lake. There is an island in the middle of the lake.\nType \"wait\" to wait for a boat. Type \"swim\" to swim across. ")
+  choice_2 = input("You've come to a lake. There is an island in the middle of the lake.\nType \"wait\" to wait for a boat. Type \"swim\" to swim across.\n")
   if choice_2.lower() == "wait":
-    choice_3 = input("You arrive at the island unharmed. There is a house with 3 doors.\nOne red, one yellow and one blue. Which colour do you choose? ")
-    if choice_3.lower() == "yellow":
+    choice_3 = input("You arrive at the island unharmed. There is a house with 3 doors.\nOne red, one yellow and one blue. Which colour do you choose?\n").lower()
+    if choice_3 == "yellow":
       print("YOU WIN!")
-    elif choice_3.lower() == "red":
+    elif choice_3 == "red":
       print("Bitten by a gang of komodo dragons.\nGAME OVER.")
-    elif choice_3.lower() == "blue":
+    elif choice_3 == "blue":
       print("Sprayed by molten lava.\nGAME OVER.")
     else:
       print("No treasure for you.\nGAME OVER.")
